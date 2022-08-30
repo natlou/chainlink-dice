217a3339dacb7f1b43ff006d5b555990d5fd9b8f
natlou <natloudev@gmail.com>
2022-08-29 19:56:04 -0700

    notes for DiceGame contract

--- packages/hardhat-ts/contracts/DiceGame.sol
+++ packages/hardhat-ts/contracts/DiceGame.sol
@@ -13,31 +13,39 @@ contract DiceGame {
     event Winner(address winner, uint256 amount);
 
     constructor() payable {
+        // can deposit initial prize
         resetPrize();
     }
 
     function resetPrize() private {
+        // initial prize is 10% of the balance (smart math) 
         prize = ((address(this).balance * 10) / 100);
     }
 
     function rollTheDice() public payable {
+        // makes sure roll is above 0.002 ether 
         require(msg.value >= 0.002 ether, "Failed to send enough value");
 
+        // gets the blockhash of the previous block (current blockhash is not revealed)
         bytes32 prevHash = blockhash(block.number - 1);
-        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), nonce));
+        // turns prevHash, address(this), nonce (in that order) to a hash: 0x(prevHash)(address(this))(none)
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(this), nonce)); // abi.encodePacked() can be decoded
+        // turns the hash into an int and takes the mod of 16
         uint256 roll = uint256(hash) % 16;
 
         console.log("THE ROLL IS ",roll);
 
-        nonce++;
+        nonce++; //increments nonce
         prize += ((msg.value * 40) / 100);
 
         emit Roll(msg.sender, roll);
 
+        // ends contract call if player doesnt win 
         if (roll > 2 ) {
             return;
         }
 
+        // otherwise send the player the prize
         uint256 amount = prize;
         (bool sent, ) = msg.sender.call{value: amount}("");
         require(sent, "Failed to send Ether");
@@ -46,5 +54,6 @@ contract DiceGame {
         emit Winner(msg.sender, amount);
     }
 
+    // fallback payable
     receive() external payable {  }
 }
