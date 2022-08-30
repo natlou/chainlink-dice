e0a3c4f6b87fa33a50f75650051a4bda0758d185
natlou <natloudev@gmail.com>
2022-08-30 11:24:08 -0700

    rigged roll deployer

--- packages/hardhat-ts/deploy/01_deploy_riggedRoll.ts
+++ packages/hardhat-ts/deploy/01_deploy_riggedRoll.ts
@@ -10,14 +10,14 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironmentExtended) => {
 
     const DiceGameContract = await hre.deployments.get('DiceGame');
 
-    /*
+
     await deploy('RiggedRoll', {
         // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
         from: deployer,
         args: [DiceGameContract.address],
         log: true,
     });
-    */
+
 
     // Getting a previously deployed contract
     //const riggedRoll = await ethers.getContract("RiggedRoll", deployer);
