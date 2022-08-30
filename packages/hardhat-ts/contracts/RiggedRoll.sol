6750629707e9e5d85f84f8bd072b63d257a9e9c4
natlou <natloudev@gmail.com>
2022-08-30 12:43:04 -0700

    made contract only callable by owner

--- packages/hardhat-ts/contracts/RiggedRoll.sol
+++ packages/hardhat-ts/contracts/RiggedRoll.sol
@@ -14,10 +14,11 @@ contract RiggedRoll is Ownable {
   }
 
   //Add withdraw function to transfer ether from the rigged contract to an address
-  function withdraw(address _addr, uint256 _amount) public {
+  function withdraw(address _addr, uint256 _amount) public onlyOwner {
     require(_amount >= address(this).balance);
     payable(_addr).transfer(_amount);
   }
+
   //Add riggedRoll() function to predict the randomness in the DiceGame contract and only roll when it's going to be a winner
   function riggedRoll() public {
 
